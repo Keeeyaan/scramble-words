a69d8e08c43e2e9d9702052fa067cd3a6a8accf0
Keeeyaan <keanjieden@gmail.com>
2023-04-30 02:06:31 +0800

    added createNewGame functionality and update UI

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,34 +1,41 @@
-const Menu = () => {
+import { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+import { socket } from '../connections/socket';
+
+const Menu = ({ playerName, setPlayerName }: any) => {
+  const navigate = useNavigate();
+  const nameInputRef = useRef<any>();
+
+  const handleCreateNewGame = () => {
+    const newGameRoomId = uuidv4();
+    const nameInput = nameInputRef.current.value;
+
+    if (nameInput) {
+      setPlayerName(nameInput);
+    }
+
+    socket.emit('createNewGame', newGameRoomId);
+    navigate(`/${newGameRoomId}`);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen ">
-      <h1 className="text-4xl font-bold mb-8 text-white">
+      <h1 className="text-4xl font-bold mb-4 text-white">
         Scrambled Words Game
       </h1>
-      <div className="bg-[#F4EEE0] text-center p-4 rounded">
-        <h1 className="text-slate-800 text-lg mb-4 font-bold uppercase">
-          Start a new room
-        </h1>
+      <div className="justify-center gap-4 p-4 rounded w-[350px] flex flex-col">
+        <input
+          ref={nameInputRef}
+          className="p-2 rounded border font-medium"
+          placeholder={playerName}
+        />
         <button
-          className="bg-green-600 text-white font-bold py-2 px-8 rounded mb-4 transition-all hover:bg-green-500"
-          onClick={() => console.log('c')}
+          className="bg-green-600 text-white text-2xl font-bold py-2 px-8 rounded transition-all hover:bg-green-500"
+          onClick={handleCreateNewGame}
         >
-          Play
+          Play!
         </button>
-
-        <hr className="border-black" />
-        <h1 className="text-start text-slate-800 text-lg my-4 font-bold uppercase">
-          Join a private room
-        </h1>
-        <div className="flex items-center gap-2 justify-center">
-          <p>Code:</p>
-          <input className="p-2 border border-slate-500 rounded" />
-          <button
-            className="bg-blue-600 text-white transition-all font-bold py-2 px-4 rounded shadow-lg hover:bg-blue-500"
-            onClick={() => console.log('j')}
-          >
-            Join
-          </button>
-        </div>
       </div>
     </div>
   );
