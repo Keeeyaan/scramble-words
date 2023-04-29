7bbf1293ec1c3537b9323167db5d89560c134228
Keeeyaan <keanjieden@gmail.com>
2023-04-30 02:09:28 +0800

    added name generator on intial value of player name

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
+import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { uniqueNamesGenerator, names } from 'unique-names-generator';
 
 import Menu from './components/Menu';
-import Game from './components/Game';
+import Game from './components/Game/Game';
 
 const App = () => {
+  const [playerName, setPlayerName] = useState<string>(
+    uniqueNamesGenerator({
+      dictionaries: [names],
+      length: 1,
+      style: 'lowerCase',
+    })
+  );
+
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Menu />} />
-          <Route path="/game" element={<Game />} />
+          <Route
+            path="/"
+            element={
+              <Menu playerName={playerName} setPlayerName={setPlayerName} />
+            }
+          />
+          <Route path="/:gameId" element={<Game playerName={playerName} />} />
         </Routes>
       </BrowserRouter>
     </>
